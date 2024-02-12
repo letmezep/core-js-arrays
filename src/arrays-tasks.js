557da59fe78082508f5b8b26a6f3bf0063fdef64
letmezep <letmezep@gmail.com>
2024-02-12 18:14:10 +0800

    feat: solved flattenArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -274,16 +274,11 @@ function distinct(arr) {
  *    createNDimensionalArray(1, 1) => [0]
  */
 function createNDimensionalArray(n, size) {
-  // const filledArray = Array.from({ length: size }, () => 0);
-  // const newArr = Array.from({ length: size }, () => filledArray);
-  // return newArr;
   const nestedArray = Array(size).fill(0);
-
   const fillArray = (arr, depth) => {
     if (depth === n) return arr;
     return arr.map(() => fillArray([...nestedArray], depth + 1));
   };
-
   return fillArray([...nestedArray], 1);
 }
 
