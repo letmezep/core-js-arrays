0f2d1529eae65f003be929606e1c004dcff13632
letmezep <letmezep@gmail.com>
2024-02-12 17:13:37 +0800

    feat: solved getHead

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -194,8 +194,9 @@ function insertItem(arr, item, index) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  const newArray = arr.slice(0, n);
+  return newArray;
 }
 
 /**
