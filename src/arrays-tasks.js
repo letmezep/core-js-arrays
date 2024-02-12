55904264772d89c018bb1cdc216d39247ba9d450
letmezep <letmezep@gmail.com>
2024-02-12 12:00:25 +0800

    feat: solved isValueEqualIsIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -148,6 +148,12 @@ function getAverage(arr) {
  */
 function isSameLength(/* arr */) {
   throw new Error('Not implemented');
+  // let result = false;
+  // if (arr.length <= 1) {
+  //   result = true;
+  // }
+  // const firstLength = arr.length;
+  // return arr.every((item) => item.length === firstLength);
 }
 
 /**
@@ -161,8 +167,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((element, index) => element === index);
 }
 
 /**
